b27fa3fbc617a2903a0cba5362d5bbd3b68c4a56
Svetlana Astanina <astaninasv@gmail.ru>
2022-02-04 15:20:22 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const Header = () => {
+	return <header className='header'>
+		<img src='https://st.depositphotos.com/1008709/4676/i/600/depositphotos_46768767-stock-photo-hands-in-art-logo.jpg' />
+	</header>
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+const Profile = () => {
+	return <div className='content'>
+		<div>
+			<img src='https://rus-pic.ru/wp-content/uploads/2021/05/krasivye-kartinki-na-avu-40-foto-1c33460.jpg' />
+		</div>
+		<div>
+			ava
+		</div>
+		<div>
+			<div>
+				my post
+			</div>
+			<div>
+				New post
+			</div>
+			<div>
+				post 1
+			</div>
+			<div>
+				post 2
+			</div>
+		</div>
+	</div>
+}
+export default Profile;
